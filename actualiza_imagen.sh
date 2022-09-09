5576e31e0a570019a9847ffa03d7585187044323
Manel <ea3eiz@ea3eiz.com>
2022-09-09 23:46:39 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -5,7 +5,7 @@
                         #sudo rm -R C108
                         git clone http://github.com/ea3eiz/EMISORA
                         sleep 1
-                        mv C108 B108
+                        mv EMISORA B108
                         sleep 1
                         sudo chmod 777 -R B108
                         cd /home/pi/B108
