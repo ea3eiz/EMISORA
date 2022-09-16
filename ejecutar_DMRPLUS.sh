9ff0d48caf6ca04040bd6d67b2b9d8b32b76e5c7
Manel <ea3eiz@ea3eiz.com>
2022-09-16 17:52:10 +0200

    1

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -2,4 +2,4 @@
 sudo killall -9 MMDVMPLUS
 sudo sed -i '6c MMDVMPLUS=ON' /home/pi/status.ini
 cd /home/pi/MMDVMHost
-xterm -geometry 73x10+537+151 -bg white -fg black -fa ‘verdana’ -fs 8x -T CONSOLA_DMRPLUS -e sudo ./MMDVMPLUS MMDVPLUS.ini
+xterm -geometry 73x10+537+151 -bg white -fg black -fa ‘verdana’ -fs 8x -T CONSOLA_DMRPLUS -e sudo ./MMDVMPLUS MMDVMPLUS.ini
