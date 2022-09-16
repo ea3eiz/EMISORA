399befdd2a4d1466995b47f1972d020ad7931e73
Manel <ea3eiz@ea3eiz.com>
2022-09-16 17:02:24 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -9,6 +9,6 @@ echo " *************************************************************************
 sleep 2
 #/home/pi/B108/./qt_info_bm & sudo ./MMDVMBM MMDVMBM.ini 
 #sudo ./MMDVMBM MMDVMBM.ini
-xterm -geometry 76x14+80+57 -bg black -fg white -fa ‘verdana’ -fs 9x -T ACTUALIZANDO_IMAGEN -e sudo ./MMDVMBM MMDVMBM.ini
+xterm -geometry 76x16+80+57 -bg black -fg white -fa ‘verdana’ -fs 9x -T ACTUALIZANDO_IMAGEN -e sudo ./MMDVMBM MMDVMBM.ini
 
 
