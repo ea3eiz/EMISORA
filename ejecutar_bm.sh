faf54fb69960ce47dd55cc034a1de3d31b7f5d51
Manel <ea3eiz@ea3eiz.com>
2022-09-16 17:40:50 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -7,4 +7,4 @@ cd /home/pi/MMDVMHost
 #echo "                          ABRIENDO BRANDMEISTER                             "
 #echo " **************************************************************************"
 #sleep 2
-xterm -geometry 73x10+16+151 -bg black -fg white -fa ‘verdana’ -fs 8x -T ACTUALIZANDO_IMAGEN -e sudo ./MMDVMBM MMDVMBM.ini
+xterm -geometry 73x10+16+151 -bg black -fg orange -fa ‘verdana’ -fs 8x -T ACTUALIZANDO_IMAGEN -e sudo ./MMDVMBM MMDVMBM.ini
