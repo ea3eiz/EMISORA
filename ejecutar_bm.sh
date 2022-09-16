f5d81ba4359ba56303e27bf153ff9f4c488eea32
Manel <ea3eiz@ea3eiz.com>
2022-09-16 17:21:56 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-#sudo killall -9 MMDVMBM
+sudo killall -9 MMDVMBM
 sudo sed -i "7c MMDVMBM=ON" /home/pi/status.ini
 cd /home/pi/MMDVMHost
-echo "\33[38;5;138m"
-echo " **************************************************************************"
-echo "                          ABRIENDO BRANDMEISTER                             "
-echo " **************************************************************************"
-sleep 2
-#/home/pi/B108/./qt_info_bm & sudo ./MMDVMBM MMDVMBM.ini 
-#sudo ./MMDVMBM MMDVMBM.ini
-xterm -geometry 76x9+13+137 -bg black -fg white -fa ‘verdana’ -fs 9x -T ACTUALIZANDO_IMAGEN -e sudo ./MMDVMBM MMDVMBM.ini
-
-
+#echo "\33[38;5;138m"
+#echo " **************************************************************************"
+#echo "                          ABRIENDO BRANDMEISTER                             "
+#echo " **************************************************************************"
+#sleep 2
+xterm -geometry 73x10+16+151 -bg black -fg white -fa ‘verdana’ -fs 8x -T ACTUALIZANDO_IMAGEN -e sudo ./MMDVMBM MMDVMBM.ini
