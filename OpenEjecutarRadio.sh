c2b2606b4431e278a30f5f405156e65ba55d04be
Manel <ea3eiz@ea3eiz.com>
2022-09-16 18:06:31 +0200

    1

--- OpenEjecutarRadio.sh
+++ OpenEjecutarRadio.sh
@@ -2,5 +2,4 @@
 sudo killall -9 MMDVMHost
 sudo sed -i '5c MMDVM=ON' /home/pi/status.ini
 cd /home/pi/MMDVMHost
-sudo ./MMDVMHost MMDVM.ini
 xterm -geometry 73x10+1058+151 -bg black -fg blue -fa ‘verdana’ -fs 8x -T CONSOLA_RADIO -e sudo ./MMDVMHost MMDVM.ini
