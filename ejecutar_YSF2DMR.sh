94805a0eff1d79ae105b5e11ed211857da6a3af7
ea3eiz <ea3eiz@ea3eiz.com>
2022-09-22 20:06:08 +0200

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-Pcian="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
 
-echo "${Pcian}"
 cd /home/pi/YSF2DMR
-#xterm -geometry 72x6+620+504 -bg blue -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
-xterm -geometry 72x6+620+504 -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
+xterm -geometry 72x6+620+504 -bg blue -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
 
 sleep 1
 
