47f6132d1cb4b0de842388c6f1954716f7e75550
ea3eiz <ea3eiz@ea3eiz.com>
2022-09-22 18:39:54 +0200

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /home/pi/YSF2DMR
-xterm -geometry 72x7+620+466 -bg black -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
+xterm -geometry 72x7+620+546 -bg black -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
 
 sleep 1
 
 cd /home/pi/MMDVMHost
-xterm -geometry 72x9+625+632 -bg black -fg white -fa ‘verdana’ -fs 9x -T MMDVMFUSION -e sudo ./MMDVMFUSION MMDVMFUSION.ini
+xterm -geometry 72x9+620+632 -bg black -fg white -fa ‘verdana’ -fs 9x -T MMDVMFUSION -e sudo ./MMDVMFUSION MMDVMFUSION.ini
