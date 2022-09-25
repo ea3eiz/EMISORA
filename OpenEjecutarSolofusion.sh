8b9be32bc912a1562d17373b2f9c3fafaa3bdde1
Manel <ea3eiz@ea3eiz.com>
2022-09-25 17:34:40 +0200

    1

--- OpenEjecutarSolofusion.sh
+++ OpenEjecutarSolofusion.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo sed -i '12c SOLOFUSION=ON' /home/pi/status.ini
 echo "\33[38;5;138m"
 echo " **************************************************************************"
@@ -9,8 +8,6 @@ sleep 2
 cd /home/pi/YSFClients/YSFGateway
 sudo xterm -geometry 77x4+1276+56 -bg black -fg white -fa 'Verdana' -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini & 
 cd /home/pi/MMDVMHost
-#/home/pi/B108/./qt_info_solofusion & sudo ./MMDVMFUSION MMDVMFUSION.ini
-#sudo ./MMDVMFUSION MMDVMFUSION.ini
 sudo xterm -geometry 77x9+1276+151 -bg black -fg white -fa 'Verdana' -fs 9x -T MMDVMFUSION -e ./MMDVMFUSION MMDVMFUSION.ini &
 
 
--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 cd /home/pi/YSF2DMR
-xterm -geometry 72x6+620+504 -bg blue -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
+xterm -geometry 77x4+1276+56 -bg blue -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
 
 sleep 1 
 
 cd /home/pi/MMDVMHost
-xterm -geometry 72x6+620+628 -bg blue -fg white -fa ‘verdana’ -fs 9x -T MMDVMFUSION -e sudo ./MMDVMFUSION MMDVMFUSION.ini
+xterm -geometry 77x9+1276+151 -bg blue -fg white -fa ‘verdana’ -fs 9x -T MMDVMFUSION -e sudo ./MMDVMFUSION MMDVMFUSION.ini
