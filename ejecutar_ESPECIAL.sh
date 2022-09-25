5ce708456bccd781f013bb0cfe954482c4b94c10
Manel <ea3eiz@ea3eiz.com>
2022-09-25 18:42:08 +0200

    1

--- ejecutar_ESPECIAL.sh
+++ ejecutar_ESPECIAL.sh
@@ -2,4 +2,4 @@
 sudo killall -9 MMDVMESPECIAL
 sudo sed -i '10c MMDVMESPECIAL=ON' /home/pi/status.ini
 cd /home/pi/MMDVMHost
-xterm -geometry 73x10+1058+151 -bg black -fg yellow -fa ‘verdana’ -fs 8x -T CONSOLA_ESPECIAL -e sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini
+xterm -geometry 73x10+1058+151 -bg yellow -fg black -fa ‘verdana’ -fs 8x -T CONSOLA_ESPECIAL -e sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini
