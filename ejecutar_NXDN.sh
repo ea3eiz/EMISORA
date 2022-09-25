347ad590fcf7f163f527734f379b7ea45dba3341
Manel <ea3eiz@ea3eiz.com>
2022-09-25 19:48:29 +0200

    1

--- ejecutar_NXDN.sh
+++ ejecutar_NXDN.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 cd /home/pi/MMDVMHost
-xterm -geometry 77x9+1276+798 -bg black -fg white -fa ‘verdana’ -fs 9x -T MMDVMNXDN -e sudo ./MMDVMNXDN MMDVMNXDN.ini &
+xterm -geometry 77x9+1276+151 -bg Violet -fg black -fa ‘verdana’ -fs 9x -T MMDVMNXDN -e sudo ./MMDVMNXDN MMDVMNXDN.ini &
 
 sleep 1
-
 cd /home/pi/NXDNClients/NXDNGateway
-xterm -geometry 77x7+1276+662 -bg black -fg white -fa 'Verdana' -fs 9x -T NXDNGateway -e ./NXDNGateway NXDNGateway.ini
+xterm -geometry 77x4+1276+56 -bg Violet -fg black -fa 'Verdana' -fs 9x -T NXDNGateway -e ./NXDNGateway NXDNGateway.ini
 
 
 
