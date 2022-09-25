cfff0ce0e80ae15affd198179ce510a11d9c992a
Manel <ea3eiz@ea3eiz.com>
2022-09-25 19:43:39 +0200

    1

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -6,7 +6,7 @@ xterm -geometry 77x3+1276+56 -bg violet -fg black -fa 'Verdana' -fs 9x -T DMR2NX
 sleep 1
 
 cd /home/pi/MMDVMHost
-xterm -geometry 77x5+1276+209 -bg violet -fg black -fa ‘verdana’ -fs 9x -T MMDVMDMR2YSF -e sudo ./DMR2NXDN MMDVMDMR2NXDN.ini &
+xterm -geometry 77x5+1276+209 -bg violet -fg black -fa ‘verdana’ -fs 9x -T MMDVMDMR2NXDN -e sudo ./DMR2NXDN MMDVMDMR2NXDN.ini &
 
 sleep 1
 
