b5bb09b553d9a3eb642f8046d9c05938922a374d
Manel <ea3eiz@ea3eiz.com>
2022-09-25 17:55:37 +0200

    1

--- ejecutar_DMR2YSF.sh
+++ ejecutar_DMR2YSF.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/pi/DMR2YSF/
-xterm -geometry 77x2+1276+56 -bg yellow -fg black -fa 'Verdana' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
+xterm -geometry 77x2+1276+54 -bg yellow -fg black -fa 'Verdana' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
 
 sleep 1
 
