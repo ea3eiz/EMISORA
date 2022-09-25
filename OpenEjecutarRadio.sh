a2dadff809ba9dddb69e0ce85cd5a9626b58adc1
Manel <ea3eiz@ea3eiz.com>
2022-09-25 18:39:31 +0200

    1

--- OpenEjecutarRadio.sh
+++ OpenEjecutarRadio.sh
@@ -2,4 +2,4 @@
 sudo killall -9 MMDVMHost
 sudo sed -i '5c MMDVM=ON' /home/pi/status.ini
 cd /home/pi/MMDVMHost
-xterm -geometry 73x10+1058+151 -bg black -fg turquoise -fa ‘verdana’ -fs 8x -T CONSOLA_RADIO -e sudo ./MMDVMHost MMDVM.ini
+xterm -geometry 73x10+1058+151 -bg turquoise -fg white -fa ‘verdana’ -fs 8x -T CONSOLA_RADIO -e sudo ./MMDVMHost MMDVM.ini
--- OpenEjecutarSolofusion.sh
+++ OpenEjecutarSolofusion.sh
@@ -6,8 +6,8 @@ echo "                          ABRIENDO SOLO FUSION                            
 echo " **************************************************************************"
 sleep 2
 cd /home/pi/YSFClients/YSFGateway
-sudo xterm -geometry 77x4+1276+56 -bg black -fg white -fa 'Verdana' -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini & 
+sudo xterm -geometry 77x4+1276+56 -bg brown -fg white -fa 'Verdana' -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini & 
 cd /home/pi/MMDVMHost
-sudo xterm -geometry 77x9+1276+151 -bg black -fg white -fa 'Verdana' -fs 9x -T MMDVMFUSION -e ./MMDVMFUSION MMDVMFUSION.ini &
+sudo xterm -geometry 77x9+1276+151 -bg brown -fg white -fa 'Verdana' -fs 9x -T MMDVMFUSION -e ./MMDVMFUSION MMDVMFUSION.ini &
 
 
